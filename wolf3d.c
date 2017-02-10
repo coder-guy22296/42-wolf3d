20fd6029812e14e0826afb8f3c52b096c544014f
Can Yildirim <cyildiri@student.42.us.org>
2017-02-10 00:08:21 -0800

    distance to wall calculations successfull

--- wolf3d.c
+++ wolf3d.c
@@ -14,7 +14,7 @@
 #include "libgraphics.h"
 #include <fcntl.h>
 #include <stdlib.h>
-#include "math.h"
+#include <stdio.h>
 
 typedef struct	s_vec2f
 {
@@ -44,22 +44,20 @@ typedef struct	s_rc_scene
 typedef struct	s_rc_renderer
 {
     void		*mlx;
-    void		*window;
+    t_lmap      *windows;
     int			win_x;
     int			win_y;
     t_rc_scene	*scene;
 
-    void		(*render)(struct s_rc_renderer *renderer);
 }				t_rc_renderer;
 
-t_rc_renderer	*new_rc_renderer(void (*render)(t_rc_renderer *))
+t_rc_renderer	*new_rc_renderer()
 {
     t_rc_renderer	*rc_renderer;
 
     if (!(rc_renderer = (t_rc_renderer *)ft_memalloc(sizeof(t_rc_renderer))))
         return (NULL);
     rc_renderer->mlx = mlx_init();
-    rc_renderer->render = render;
     if (!(rc_renderer->scene = (t_rc_scene *)ft_memalloc(sizeof(t_rc_scene))))
         return (0);
     return (rc_renderer);
@@ -67,7 +65,13 @@ t_rc_renderer	*new_rc_renderer(void (*render)(t_rc_renderer *))
 
 void	add_rcwindow(t_rc_renderer *renderer, int width, int height, char *title)
 {
-    renderer->window = mlx_new_window(renderer->mlx, width, height, title);
+    t_lmap *to_add;
+    void    *new_window;
+
+    new_window = mlx_new_window(renderer->mlx, width, height, title);
+    to_add = ft_lmapnew(title, sizeof(title), &new_window, sizeof(new_window));
+    //printf("%p\n", new_window);
+    ft_lmapadd(&renderer->windows, to_add);
     renderer->win_x = width;
     renderer->win_y = height;
 }
@@ -276,29 +280,6 @@ t_frame *construct_map(t_rc_renderer *renderer, int **array2d, int block_size, t
     return (frame);
 }
 
-void	render(/* t_rc_renderer *renderer */)
-{
-    //
-    //	<< loop start
-    //		cast rays (player, map)
-    //		calculate slice height
-    //		draw slice to frame
-    //	<< loop end
-    // display frame
-    // destroy frame
-}
-
-
-int			render_loop(void *param)
-{
-    t_rc_renderer	*renderer;
-
-    renderer = (t_rc_renderer *)param;
-    if (renderer->scene)
-        renderer->render(renderer);
-    return (0);
-}
-
 t_player *new_player(int x, int y, float direction, int fov)
 {
     t_player *player;
@@ -333,7 +314,90 @@ int		key_pressed(int keycode, void *param)
         exit(1);
     return (0);
 }
-static void		drawray_xmajor(t_rc_renderer *renderer, t_vec2i start, t_vec2d delta)
+static void		drawray_xmajor(t_rc_renderer *renderer, char *window_name, t_vec2i start, t_vec2d delta)
+{
+    double	deltaerr;
+    double	error;
+    int     length = 0;
+    t_vec2i cur;
+    t_vec2i dir;
+    void *window;
+
+    cur = start;
+    window = *((void **)ft_lmapget(renderer->windows, window_name)->content);
+    dir.x = (delta.x < 0) ? -1 : 1;
+    dir.y = (delta.y < 0) ? -1 : 1;
+    error = -1.0;
+    deltaerr = fabs(delta.x / delta.y);
+    while (1)
+    {
+        if (get_pixel(renderer->scene->map, cur.x, cur.y) != 0)
+            break;
+        if (cur.y == start.y)
+            error += deltaerr;
+        mlx_pixel_put(renderer->mlx, window, cur.x, cur.y, 0x000000FF);
+        error += deltaerr;
+        if (error >= 0.0)
+        {
+            cur.x += dir.x;
+            error -= 1.0;
+        }
+        cur.y += dir.y;
+        length++;
+    }
+}
+
+static void		drawray_ymajor(t_rc_renderer *renderer, char *window_name, t_vec2i start, t_vec2d delta)
+{
+    double	deltaerr;
+    double	error;
+    t_vec2i cur;
+    t_vec2i dir;
+    void *window;
+
+    cur = start;
+    window = *((void **)ft_lmapget(renderer->windows, window_name)->content);
+    dir.x = (delta.x < 0) ? -1 : 1;
+    dir.y = (delta.y < 0) ? -1 : 1;
+    error = -1.0;
+    deltaerr = fabs(delta.y / delta.x);
+    error += deltaerr;
+    while (1)
+    {
+        if (get_pixel(renderer->scene->map, cur.x, cur.y) != 0)
+            break;
+        mlx_pixel_put(renderer->mlx, window, cur.x, cur.y, 0x000000FF);
+        error += deltaerr;
+        if (error >= 0.0)
+        {
+            cur.y += dir.y;
+            error -= 1.0;
+        }
+        cur.x += dir.x;
+    }
+}
+void draw_player_ray(t_rc_renderer *renderer, char *window_name)
+{
+    t_vec2i start;
+    t_player *player;
+    t_vec2d delta;
+
+
+    player = renderer->scene->player;
+    start.x = player->position.x;
+    start.y = player->position.y;
+    delta.x = cos(player->direction);
+    delta.y = sin(player->direction);
+
+    if ((fabs(delta.y / delta.x) > 1.0f || delta.x == 0))
+        drawray_xmajor(renderer, window_name, start, delta);
+    else if (fabs(delta.y / delta.x) <= 1.0)
+        drawray_ymajor(renderer, window_name, start, delta);
+
+
+}
+
+static float		castray_xmajor(t_frame *map, t_vec2i start, t_vec2d delta)
 {
     double	deltaerr;
     double	error;
@@ -348,11 +412,10 @@ static void		drawray_xmajor(t_rc_renderer *renderer, t_vec2i start, t_vec2d delt
     deltaerr = fabs(delta.x / delta.y);
     while (1)
     {
-        if (get_pixel(renderer->scene->map, cur.x, cur.y) != 0)
+        if (get_pixel(map, cur.x, cur.y) != 0)
             break;
         if (cur.y == start.y)
             error += deltaerr;
-        mlx_pixel_put(renderer->mlx, renderer->window, cur.x, cur.y, 0x000000FF);
         error += deltaerr;
         if (error >= 0.0)
         {
@@ -362,9 +425,10 @@ static void		drawray_xmajor(t_rc_renderer *renderer, t_vec2i start, t_vec2d delt
         cur.y += dir.y;
         length++;
     }
+    return ((cur.y - (float)start.y)/(float)delta.y);
 }
 
-static void		drawray_ymajor(t_rc_renderer *renderer, t_vec2i start, t_vec2d delta)
+static float		castray_ymajor(t_frame *map, t_vec2i start, t_vec2d delta)
 {
     double	deltaerr;
     double	error;
@@ -379,9 +443,8 @@ static void		drawray_ymajor(t_rc_renderer *renderer, t_vec2i start, t_vec2d delt
     error += deltaerr;
     while (1)
     {
-        if (get_pixel(renderer->scene->map, cur.x, cur.y) != 0)
+        if (get_pixel(map, cur.x, cur.y) != 0)
             break;
-        mlx_pixel_put(renderer->mlx, renderer->window, cur.x, cur.y, 0x000000FF);
         error += deltaerr;
         if (error >= 0.0)
         {
@@ -390,42 +453,120 @@ static void		drawray_ymajor(t_rc_renderer *renderer, t_vec2i start, t_vec2d delt
         }
         cur.x += dir.x;
     }
+    return ((cur.x - (float)start.x)/(float)delta.x);
 }
-void draw_player_ray(t_rc_renderer *renderer)
+
+float cast_ray(t_frame *map, t_vec2i position, double direction)
 {
     t_vec2i start;
-    t_player *player;
     t_vec2d delta;
+    float distance;
 
-
-    player = renderer->scene->player;
-    start.x = player->position.x;
-    start.y = player->position.y;
-    delta.x = cos(player->direction);
-    delta.y = sin(player->direction);
+    start.x = position.x;
+    start.y = position.y;
+    delta.x = cos(direction);
+    delta.y = sin(direction);
+    distance = 0.0;
 
     if ((fabs(delta.y / delta.x) > 1.0f || delta.x == 0))
-        drawray_xmajor(renderer, start, delta);
+        distance = castray_xmajor(map, start, delta);
     else if (fabs(delta.y / delta.x) <= 1.0)
-        drawray_ymajor(renderer, start, delta);
+        distance = castray_ymajor(map, start, delta);
 
-
+    return (distance);
 }
 
-
-void	render_map( t_rc_renderer *renderer)
+void	render_minimap( t_rc_renderer *renderer)
 {
-    mlx_clear_window(renderer->mlx, renderer->window);
+    void *window;
+
+    window = *((void **)ft_lmapget(renderer->windows, "minimap")->content);
+
+    //exit (1);
+    mlx_clear_window(renderer->mlx, window);
+
     //display map image for testing purposes
-    mlx_put_image_to_window(renderer->mlx, renderer->window, renderer->scene->map->id, 0, 0);
+    mlx_put_image_to_window(renderer->mlx, window, renderer->scene->map->id, 0, 0);
 
-    draw_player_ray(renderer);
+    draw_player_ray(renderer, "minimap");
 
-    mlx_pixel_put(renderer->mlx, renderer->window, renderer->scene->player->position.x,
+    mlx_pixel_put(renderer->mlx, window, renderer->scene->player->position.x,
                   renderer->scene->player->position.y,
                   0x00FF0000);
 }
 
+void draw_column(t_rc_renderer *renderer, int col_num, int length)
+{
+    int i;
+
+    i=0;
+    while (i < length)
+    {
+        draw_pixel(renderer->scene->cur_frame, col_num, i, 0x00FFFFFF);
+        i++;
+    }
+}
+
+void	render_player_view(t_rc_renderer *renderer)
+{
+    t_player *player;
+    int slice_height;
+    float distance;
+    int i;
+    void *window;
+
+    window = *((void **)ft_lmapget(renderer->windows, "Player View")->content);
+    //printf("%p\n", window);
+    //ft_putstr("a\n");
+    renderer->scene->cur_frame->id = mlx_new_image(renderer->mlx, renderer->win_x, renderer->win_y);
+    //ft_putstr("b\n");
+    i = 0;
+    player = renderer->scene->player;
+    if (renderer)
+        ft_putstr("\0");
+    //
+    //	<< loop start
+    while (i < renderer->win_x) {
+        //		cast rays (player, map)
+        distance = cast_ray(renderer->scene->map, player->position, player->direction);
+        //ft_putnbr((int) distance);
+        //ft_putchar('\n');
+        //		calculate slice height
+        slice_height = renderer->win_y / distance * cos(player->direction);
+        //ft_putnbr(slice_height);
+        //ft_putchar('\n');
+        //		draw slice to frame
+        //mlx_pixel_put(renderer->mlx, window, 50, 50, 0x00FF0000);
+        draw_pixel(renderer->scene->cur_frame, 50, 50, 0x00FF0000);
+        draw_column(renderer, i, slice_height * 2);
+        //	<< loop end
+        i++;
+    }
+    //ft_putstr("c\n");
+    // display frame
+
+    mlx_put_image_to_window(renderer->mlx, window, renderer->scene->cur_frame->id, 0, 0);
+
+    //ft_putstr("d\n");
+    // destroy frame
+    mlx_destroy_image(renderer->mlx, renderer->scene->cur_frame->id);
+
+    //ft_putstr("e\n");
+}
+
+int			render_loop(void *param)
+{
+    t_rc_renderer	*renderer;
+
+    renderer = (t_rc_renderer *)param;
+    if (renderer->scene)
+    {
+
+        render_player_view(renderer);
+        render_minimap(renderer);
+    }
+    return (0);
+}
 int main(int argc, char **argv)
 {
     int			    **array2d;
@@ -441,37 +582,39 @@ int main(int argc, char **argv)
     }
     array2d = NULL;
     row_col = new_vec2i(0,0);
-
+ft_putstr("test\n");
     //initalize ray caster
-    rc_renderer = new_rc_renderer(render_map);
-
+    rc_renderer = new_rc_renderer();
     //load map to a 2d array
     array2d = load_map(argv[1], row_col);
-
+    ft_putstr("test\n");
     //add window
-    add_rcwindow(rc_renderer, row_col->x * block_size, row_col->y * block_size, "minimap");
-
+    add_rcwindow(rc_renderer, row_col->x * block_size, row_col->y * block_size, ft_strdup("minimap"));
+    add_rcwindow(rc_renderer, 1000, 1000, ft_strdup("Player View"));
+    ft_putstr("test\n");
 
     //construct an image map that will be used for casting rays
     rc_renderer->scene->map = construct_map(rc_renderer, array2d, block_size, row_col);
 
+    rc_renderer->scene->cur_frame = new_tframe(rc_renderer, rc_renderer->win_x, rc_renderer->win_y);
+
+    ft_putstr("test\n");
     //free the memory used for the 2d array
     del_intArr(array2d, row_col->y);
-
+    ft_putstr("test\n");
     //add a player
     rc_renderer->scene->player = new_player(50, 50, 0, 60);
 
-    //display map image for testing purposes
-    mlx_put_image_to_window(rc_renderer->mlx, rc_renderer->window, rc_renderer->scene->map->id, 0, 0);
 
-    mlx_pixel_put(rc_renderer->mlx, rc_renderer->window, rc_renderer->scene->player->position.x,
-                  rc_renderer->scene->player->position.y,
-                  0x00FF0000);
-    draw_player_ray(rc_renderer);
+    draw_pixel(rc_renderer->scene->cur_frame, 50, 50, 0x00FF0000);
 
-    mlx_hook(rc_renderer->window, 2, 0, key_pressed, rc_renderer);
 
+    ft_putstr("test\n");
+    mlx_hook(*((void **)ft_lmapget(rc_renderer->windows, "minimap")->content), 2, 0, key_pressed, rc_renderer);
+    mlx_hook(*((void **)ft_lmapget(rc_renderer->windows, "Player View")->content), 2, 0, key_pressed, rc_renderer);
+    ft_putstr("test\n");
     mlx_loop_hook(rc_renderer->mlx, render_loop, rc_renderer);
+    ft_putstr("test\n");
     // run the mlx loop
     mlx_loop(rc_renderer->mlx);
 
